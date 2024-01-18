7f3754a2eb812d586c1efd62f3e03717aef8456a
Francesco <napo.fra96@gmail.com>
2024-01-18 14:21:04 +0100

    first commit
    create react-app | with typescript | create interface

---
+++ ts-project/src/types/interfaces.ts
@@ -0,0 +1,21 @@
+interface IArticle {
+  id: number;
+  title: string;
+  url: string;
+  image_url: string;
+  news_site: string;
+  summary: string;
+  published_at: string;
+  updated_at: string;
+  featured: boolean;
+  launches: Array<any>;
+  events: Array<any>;
+}
+
+export interface IArticleProps {
+  article: IArticle;
+}
+
+export interface IArticleDetailProps {
+  id: number;
+}
